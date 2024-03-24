c3dbef3caee141a59f5f0a1a815e9c2b812331e1
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-03-24 13:32:52 +0600

    PDO first steps

--- dynamic-web-application/index.php
+++ dynamic-web-application/index.php
@@ -1,3 +1,18 @@
 <?php
 require("functions.php");
-require("router.php");
+//require("router.php");
+
+//connect to our mySQL database
+$dsn = "mysql:host=localhost;port=3306;dbname=php_tutorial_jw;user=root;password=;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post) {
+//    echo "<li>" . $post['title'] . "</li>";
+    echo "<li>{$post['title']}</li>";
+}
+
