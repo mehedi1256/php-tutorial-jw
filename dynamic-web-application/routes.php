809396ea3ddbecf3286eb6f05553218432152493
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-04-27 23:49:55 +0600

    updated with path request

--- dynamic-web-application/routes.php
+++ dynamic-web-application/routes.php
@@ -5,6 +5,8 @@ $router->get('/dynamic-web-application/about', 'controllers/about.php');
 $router->get('/dynamic-web-application/notes', 'controllers/notes/index.php');
 $router->get('/dynamic-web-application/note', 'controllers/notes/show.php');
 $router->delete('/dynamic-web-application/note', 'controllers/notes/destroy.php');
+$router->get('/dynamic-web-application/note/edit', 'controllers/notes/edit.php');
+$router->patch('/dynamic-web-application/note', 'controllers/notes/update.php');
 $router->get('/dynamic-web-application/note/create', 'controllers/notes/create.php');
 $router->post('/dynamic-web-application/note/create', 'controllers/notes/store.php');
 $router->get('/dynamic-web-application/contact', 'controllers/contact.php');
--- dynamic-web-application/views/notes/show.view.php
+++ dynamic-web-application/views/notes/show.view.php
@@ -6,15 +6,14 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p class="mb-6">
-            <a href="/dynamic-web-application/notes" class="text-blue-500 underline">Go Back...</a>
+            <a href="/dynamic-web-application/note" class="text-blue-500 underline">Go Back...</a>
         </p>
         <p><?= htmlspecialchars($note['body']) ?></p>
 
-        <form class="mt-6" method="POST">
-            <input type="hidden" name="_method" value="DELETE" />
-            <input type="hidden" name="id" value="<?= $note['id'] ?>" />
-            <button class="text-sm text-red-500">Delete</button>
-        </form>
+
+        <footer class="mt-6">
+            <a href="/dynamic-web-application/note/edit?id=<?= $note['id'] ?>" class="rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Edit</a>
+        </footer>
     </div>
 </main>
 
