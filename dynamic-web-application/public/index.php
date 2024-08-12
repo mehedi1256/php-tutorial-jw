1b7bde74805a3e7a019ed69720d44c3e88fcb5d3
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-08-12 18:45:35 +0600

    composer and autoloading file

--- dynamic-web-application/public/index.php
+++ dynamic-web-application/public/index.php
@@ -3,16 +3,13 @@
 use core\Session;
 use core\ValidationException;
 
-session_start();
-
 const BASE_PATH = __DIR__ . '/../';
 
-require BASE_PATH . "core/functions.php";
+require BASE_PATH . 'vendor/autoload.php';
 
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', '/', $class);
-    require base_path("{$class}.php");
-});
+session_start();
+
+require BASE_PATH . "core/functions.php";
 
 require base_path('bootstrap.php');
 
@@ -20,7 +17,6 @@ $router = new \core\Router();
 
 $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
-
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 try {
