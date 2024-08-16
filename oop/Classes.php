d7318a29878f7143c1d0611f743fdfa8426a6c61
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-08-16 23:59:05 +0600

    oop

---
+++ oop/Classes.php
@@ -0,0 +1,23 @@
+<?php
+
+class Invoice
+{
+
+}
+
+class InvoiceItems
+{
+    
+}
+
+class AcievementBadge
+{
+    public $title;
+    public $description;
+    public $points;
+
+    public function awardTo($user)
+    {
+
+    }
+}
